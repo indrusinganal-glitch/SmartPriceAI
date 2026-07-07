17c2d49cbefb4417141ee6e9c123ff7045b605c0
Indru Singanal <indrusinganal@gmail.com>
2026-07-07 10:49:21 +0530

    Add whitenoise for static files

--- smartprice/settings.py
+++ smartprice/settings.py
@@ -36,6 +36,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
 ROOT_URLCONF = "smartprice.urls"
@@ -101,19 +102,14 @@ USE_TZ = True
 
 STATIC_URL = "/static/"
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
-
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-
-# Media files
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 MEDIA_URL = "/media/"
-
 MEDIA_ROOT = BASE_DIR / "media"
-
 
 # Default primary key field type
 
